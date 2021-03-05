ebc7defaf14d2dcf3929a3553bf65cae331b6b8c
John Helveston <john.helveston@gmail.com>
2021-03-05 13:52:06 -0500

    updated styling colors

--- _site/R/functions.R
+++ _site/R/functions.R
@@ -1,25 +1,43 @@
 
-# Function for generating the html used to create the link cards
-# on my teaching page: https://jhelvy.github.io/teaching
+# Function for generating the html used to create a two-column layout 
+# with a thumbnail image, a title, and some text:
+#
+# |       | title
+# | image | text
+# |       | subtext (smaller font than text)
+#
+# See example on my teaching page: https://jhelvy.github.io/teaching
 
-make_link_card <- function(
+make_title_card <- function(
   title = NULL,
-  subtitle = NULL,
-  description = NULL,
+  text = NULL,
+  subtext = NULL,
   url = NULL,
   image_src = NULL,
   image_width = 120
 ) {
 
-  x <- htmltools::HTML(paste0(
-    '<div>
-        <img src="', image_src, '" style="float:left; width:', image_width, 
-    'px;">',
-        '<h3 style="margin-bottom: 5px;"><a href=', url, '>', title, '</a></h3>',
-        '<p style="margin-bottom: 5px;">', subtitle, '</p>',
-        '<span style="font-size:0.8rem;">', description, '</span>',
-    '</div>'
-  ))
-
-  return(x)
+  if (is.null(url)) {
+    html <- htmltools::HTML(paste0(
+      '<div>
+          <img src="', image_src, '" style="float:left; width:', image_width, 
+      'px; margin: 0 15px 0 0">',
+          '<h3 style="margin-bottom: 5px;">', title, '</h3>',
+          '<p style="margin-bottom: 5px;">',  text, '</p>',
+          '<span style="font-size:0.8rem;">', subtext, '</span>',
+      '</div>'
+    ))
+  } else {
+    html <- htmltools::HTML(paste0(
+      '<div>
+          <img src="', image_src, '" style="float:left; width:', image_width, 
+      'px; margin: 0 15px 0 0">',
+          '<h3 style="margin-bottom: 5px;"><a href=', url, '>', title, '</a></h3>',
+          '<p style="margin-bottom: 5px;">',  text, '</p>',
+          '<span style="font-size:0.8rem;">', subtext, '</span>',
+      '</div>'
+    ))
+  }
+  
+  return(html)
 }
