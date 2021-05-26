d3752438c670d7aac82ac87069e89efd3c4c68eb
John Helveston <john.helveston@gmail.com>
2021-05-26 07:43:55 -0400

    updated pubs

--- R/functions.R
+++ R/functions.R
@@ -1,9 +1,11 @@
+library(htmltools)
+
 aside <- function(text) {
-  return(htmltools::tag("aside", list(text)))
+  return(tag("aside", list(text)))
 }
 
 center <- function(text) {
-  return(htmltools::tag("center", list(text)))
+  return(tag("center", list(text)))
 }
 
 aside_center <- function(text) {
@@ -11,29 +13,26 @@ aside_center <- function(text) {
 }
 
 aside_center_b <- function(text) {
-  return(aside(center(list(htmltools::tag("b", text)))))
+  return(aside(center(list(tag("b", text)))))
 }
 
 haiku <- function(one, two, three) {
   return(aside_center(list(
-    htmltools::em(
-      one, htmltools::br(),
-      two, htmltools::br(),
-      three)
+    em(one, HTML("&#8226;"), two, HTML("&#8226;"), three)
   )))
 }
 
 markdown_to_html <- function(text) {
   if (is.null(text)) { return(text) }
-  return(htmltools::HTML(markdown::renderMarkdown(text = text)))
+  return(HTML(markdown::renderMarkdown(text = text)))
 }
 
 make_icon <- function(icon) {
-  return(htmltools::tag("i", list(class = icon)))
+  return(tag("i", list(class = icon)))
 }
 
 make_icon_text <- function(icon, text) {
-  return(htmltools::HTML(paste0(make_icon(icon), " ", text)))
+  return(HTML(paste0(make_icon(icon), " ", text)))
 }
 
 # Creates the html to make a button to an external link
@@ -41,12 +40,12 @@ icon_link <- function(icon = NULL, text = NULL, url = NULL) {
   if (!is.null(icon)) {
     text <- make_icon_text(icon, text)
   }
-  return(htmltools::a(href = url, text, class = "icon-link"))
+  return(a(href = url, text, class = "icon-link"))
 }
 
 doi <- function(doi) {
   url <- paste0('https://doi.org/', doi)
-  return(paste0('DOI: ', htmltools::a(href = url, doi)))
+  return(paste0('DOI: ', a(href = url, doi)))
 }
 
 gscholar_stats <- function(url) {
@@ -73,9 +72,9 @@ get_cites <- function(url) {
 # https://jhelvy.github.io/projects
 
 masonry_grid <- function(...) {
-  return(htmltools::div(
+  return(div(
     class = "masonry-wrapper",
-    htmltools::div(class = "masonry", ...)
+    div(class = "masonry", ...)
   ))
 }
 
@@ -85,19 +84,19 @@ masonry_item <- function(
   description = NULL,
   url = NULL
 ) {
-  image <- htmltools::img(src = src)
+  image <- img(src = src)
   if (!is.null(url)) {
-    image <- htmltools::a(
+    image <- a(
       href = url,
       class = "card-hover",
-      htmltools::img(src = src))
+      img(src = src))
   }
-  return(htmltools::div(class = "masonry-item",
-    htmltools::div(
+  return(div(class = "masonry-item",
+    div(
       class = "masonry-content",
       image,
-      htmltools::h3(class = "masonry-title", title),
-      htmltools::p(class = "masonry-description", description)
+      h3(class = "masonry-title", title),
+      p(class = "masonry-description", description)
     )
   ))
 }
@@ -107,13 +106,13 @@ create_footer <- function() {
   fill <- '#ededeb'
   height <- '14px'
 
-  footer <- htmltools::HTML(paste0(
+  footer <- HTML(paste0(
   '© 2021 John Paul Helveston [',
   fontawesome::fa('creative-commons', fill = fill, height = height),
   fontawesome::fa('creative-commons-by', fill = fill, height = height),
   fontawesome::fa('creative-commons-sa', fill = fill, height = height),
   '](https://creativecommons.org/licenses/by-sa/4.0/)\n',
-  htmltools::br(),
+  br(),
   fontawesome::fa('wrench', fill = fill, height = height), ' Made with ',
   fontawesome::fa('heart', fill = fill, height = height), ', [',
   fontawesome::fa('code-branch', fill = fill, height = height),
@@ -121,7 +120,7 @@ create_footer <- function() {
   fontawesome::fa('r-project', fill = fill, height = height),
   '](https://cran.r-project.org/) ',
   '[distill](https://github.com/rstudio/distill) package\n',
-  htmltools::br(),
+  br(),
   last_updated(), "\n\n",
 
   '<!-- Add function to open links to external links in new tab, from: -->',
@@ -133,7 +132,7 @@ create_footer <- function() {
 }
 
 last_updated <- function() {
-  return(htmltools::span(
+  return(span(
     paste0(
       'Last updated on ',
       format(Sys.Date(), format="%B %d, %Y")
