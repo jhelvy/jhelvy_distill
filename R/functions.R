dbb0aa75ee64efa2edff5f88c7420a85cac0b4a3
jhelvy <john.helveston@gmail.com>
2021-07-19 15:40:34 +0000

    Deploying to gh-pages from  @ eebbaef344f748465c032943ddbd30257824f11b 🚀

--- R/functions.R
+++ R/functions.R
@@ -68,14 +68,117 @@ get_cites <- function(url) {
   return(cites)
 }
 
-# Masonry layout for projects page:
-# https://jhelvy.github.io/projects
+# Functions for projects page: https://jhelvy.github.io/projects
 
-get_categories <- function(projects) {
-  return(setdiff(
-    names(projects), 
-    c("title", "src", "url", "description")))  
+make_posts_page <- function(posts) {
+  categories <- get_post_categories(posts)
+  posts <- unite_post_categories(posts, categories)
+  posts_html <- make_posts_html(posts)
+  cats_html <- make_cats_html(categories)
+  return(
+    div(class = "posts-container posts-with-sidebar posts-apply-limit l-screen-inset", 
+      posts_html,
+      cats_html
+  ))
 }
+
+get_post_categories <- function(posts) {
+  categories <- posts %>% 
+    select(-c("title", "src", "url", "description")) %>% 
+    pivot_longer(
+      cols = everything(),
+      names_to = "category",
+      values_to = "count") %>% 
+    group_by(category) %>% 
+    summarise(count = sum(count)) %>% 
+    arrange(desc(count))
+  return(categories)
+}
+
+unite_post_categories <- function(posts, categories) {
+  posts <- posts %>% 
+    pivot_longer(
+      names_to = "categories", 
+      values_to = "val", 
+      cols = categories$category) %>% 
+    mutate(val = ifelse(val == 1, categories, NA_character_)) %>% 
+    pivot_wider(
+      names_from = categories, 
+      values_from = val) %>% 
+    unite(
+      -c('title', 'src', 'url', 'description'), 
+      col = "categories", sep = ";", na.rm = T)
+  return(posts)
+}
+
+make_posts_html <- function(posts) {
+  posts_content <- list()
+  for (i in seq_len(nrow(posts))) {
+    posts_content[[i]] <- make_post_content(posts[i,])
+  }
+  return(div(class = "posts-list", posts_content))
+}
+
+make_post_content <- function(post) {
+  post_cats <- strsplit(post$categories, ";")[[1]]
+  post_tags <- get_post_tags(post_cats)
+  return(a(
+    href = post$url,
+    class = "post-preview",
+    HTML(
+      paste0(
+        '<script class="post-metadata" type="text/json">{"categories":[',
+        paste0(paste0('"', post_cats, '"'), collapse = ","),
+        ']}</script>'
+      )
+    ),
+    div(class = "metadata", div(class = "publishedDate")),
+    div(class = "thumbnail", img(src = post$src)),
+    div(class = "description",
+      h2(post$title),
+      post_tags,
+      p(post$description)
+    )
+  ))
+}
+
+get_post_tags <- function(post_cats) {
+  cats <- list()
+  for (i in seq_len(length(post_cats))) {
+    cats[[i]] <- div(class = "dt-tag", post_cats[i])
+  }
+  return(div(class = "dt-tags", cats))
+}
+
+make_cats_html <- function(categories) {
+  return(div(class = "posts-sidebar",
+    div(class = "sidebar-section categories",
+      h3("Categories"),
+      make_cats_list(categories)
+    )
+  ))
+}
+
+make_cats_list <- function(categories) {
+  cats_list <- list()
+  for (i in seq_len(nrow(categories))) {
+    cats_list[[i]] <- tags$li(
+      a(
+        href = paste0("#category:", categories$category[i]),
+        categories$category[i]
+      ),
+      HTML(paste0(
+        '<span class="category-count">(', 
+        categories$count[i], ')</span>'
+      ))
+    )
+  }
+  return(tags$ul(cats_list))
+}
+
+
+
+
 
 masonry_buttons <- function(categories) {
   cat_button <- tagList(lapply(categories, function(x) {
